3d1933b9a6532874ba29e39b13c51706bf1ce727
Dmitry Agapitov <dmitry.agapitov@blueprintsys.com>
2017-07-31 16:12:27 -0400

    RabbitMQ certificate auth sample

---
+++ NsbRabbitmqAuthTest/Program.cs
@@ -0,0 +1,87 @@
+﻿using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+namespace NsbRabbitmqAuthTest
+{
+    class Program
+    {
+        private const string QueueName = "Sample.RabbitMQ.Commands";
+
+        static void Main()
+        {
+            AsyncMain().GetAwaiter().GetResult();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        static async Task AsyncMain()
+        {
+            var endpointConfiguration = new EndpointConfiguration(
+                endpointName: QueueName);
+
+            endpointConfiguration.UseSerialization<JsonSerializer>();
+            endpointConfiguration.EnableInstallers();
+
+            var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+            var connectionString =
+                //@"Host=mac-rabbitmq;UseTls=true;Username=ImageGenClient;Password=MySecretPassword";
+                "Host=mac-rabbitmq;UseTls=true;CertPath=C:\\gh\\ssl.pfx;CertPassphrase=MySecretPassword";
+
+            transport.ConnectionString(connectionString);
+
+            endpointConfiguration.UsePersistence<InMemoryPersistence>();
+
+            try
+            {
+                // Initialize the endpoint with the finished configuration
+                var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                    .ConfigureAwait(false);
+                try
+                {
+                    await SendOrder(endpointInstance)
+                        .ConfigureAwait(false);
+                }
+                finally
+                {
+                    await endpointInstance.Stop()
+                        .ConfigureAwait(false);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex}");
+            }
+        }
+
+        static async Task SendOrder(IEndpointInstance endpointInstance)
+        {
+            Console.WriteLine("Press any key to exit");
+
+            var id = Guid.NewGuid();
+
+            var placeOrder = new PlaceOrder
+            {
+                Id = id
+            };
+            await endpointInstance.Send(QueueName, placeOrder)
+                .ConfigureAwait(false);
+            Console.WriteLine($"Sent a PlaceOrder message with id: {id:N}");
+        }
+    }
+    public class PlaceOrder : ICommand
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class PlaceOrderHandler :
+    IHandleMessages<PlaceOrder>
+    {
+        public Task Handle(PlaceOrder message, IMessageHandlerContext context)
+        {
+            Console.WriteLine($"PlaceOrder {message.Id}");
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ RabbitmqAuthTest/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net.Security;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using RabbitMQ.Util;
+
+namespace RabbitMQTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            try
+            {
+
+                var hostName = "mac-rabbitmq";
+                var cf = new ConnectionFactory
+                {
+                    HostName = hostName,
+                    VirtualHost = "/",
+                    // Client certificate authentication doesn't work without next line (can't find in NServiceBus.RabbitMQ)
+                    AuthMechanisms = new AuthMechanismFactory[] { new ExternalMechanismFactory() },
+                    Ssl = new SslOption
+                    {
+                        Enabled = true,
+                        ServerName = hostName,
+                        //Might need next lines uncommented for self-signed certificates
+                        AcceptablePolicyErrors = SslPolicyErrors.RemoteCertificateNameMismatch |
+                                                SslPolicyErrors.RemoteCertificateChainErrors,
+                        CertPath = @"C:\gh\ssl.pfx",
+                        CertPassphrase = "MySecretPassword"
+                    }
+                };
+
+                using (IConnection conn = cf.CreateConnection("ConsoleApp"))
+                {
+                    using (IModel ch = conn.CreateModel())
+                    {
+                        ch.QueueDeclare("rabbitmq-dotnet-test", false, false, false, null);
+                        ch.BasicPublish("", "rabbitmq-dotnet-test", null,
+                            Encoding.UTF8.GetBytes("Hello, World"));
+                        BasicGetResult result = ch.BasicGet("rabbitmq-dotnet-test", true);
+                        if (result == null)
+                        {
+                            Console.WriteLine("No message received.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Received:");
+                            DebugUtil.DumpProperties(result, Console.Out, 0);
+                        }
+                        ch.QueueDelete("rabbitmq-dotnet-test");
+                    }
+                }
+            }
+            catch (BrokerUnreachableException bex)
+            {
+                Exception ex = bex;
+                while (ex != null)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("inner:");
+                    ex = ex.InnerException;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
